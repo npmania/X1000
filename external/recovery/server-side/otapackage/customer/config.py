a98bee4cbede1f5a254d84f56855bee692d1fd5d
npmania <np@mkv.li>
2024-04-20 04:05:13 +0900

    modify config for Shanling M0 Pro

--- external/recovery/server-side/otapackage/customer/config.py
+++ external/recovery/server-side/otapackage/customer/config.py
@@ -2,25 +2,16 @@
 # exclusively ####
 search_prefix = 're_'
 # nor flash configuation
-re_nor_tag = 'nor'
-re_nor_device_size = '16MB'
-re_nor_partition = (
-    ("uboot", "0x0", "0x40000", "mtdblock0"),
-    ("kernel", "0x40000", "0x300000", "mtdblock1"),
-    ("rootfs", "0x360000", "0xca0000", "mtdblock2"),
-)
-# nand flash configuation
 re_nand_tag = 'nand'
-re_nand_device_size = '128MB'
+re_nand_device_size = '8MB'
 re_nand_partition = (
                     ("uboot", "0x0", "0x100000", "mtdblock0"),
-                    ("kernel", "0x100000", "0x800000", "mtdblock1"),
-                    ("rootfs", "0x980000", "0x2800000", "mtdblock2"),
-                    ("data", "0x3180000", "0x4e80000", "mtdblock3"),
+                    ("bt_mac", "0x100000", "0x20000", "mtdblock1"),
+                    ("user_id", "0x120000", "0x20000", "mtdblock2"),
+                    ("kernel", "0x140000", "0x600000", "mtdblock3"),
+                    ("recovery", "0x740000", "0x800000", "mtdblock4"),
+                    ("rootfs", "0xf40000", "0x5000000", "mtdblock5"),
+                    ("rootfs", "0x5f40000", "0x20c0000", "mtdblock6"),
 )
-# mmc configuation
-re_mmc_tag = 'mmc'
-re_mmc_device_size = '512MB'
-re_mmc_partition = ()
 
 local = locals()
