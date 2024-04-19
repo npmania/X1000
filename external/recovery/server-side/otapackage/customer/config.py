25c0e16c3b344d9bbaa3a960816b39de8e31cb7b
npmania <np@mkv.li>
2024-04-20 05:47:17 +0900

    correct config data

--- external/recovery/server-side/otapackage/customer/config.py
+++ external/recovery/server-side/otapackage/customer/config.py
@@ -3,7 +3,7 @@
 search_prefix = 're_'
 # nor flash configuation
 re_nand_tag = 'nand'
-re_nand_device_size = '8MB'
+re_nand_device_size = '128MB'
 re_nand_partition = (
                     ("uboot", "0x0", "0x100000", "mtdblock0"),
                     ("bt_mac", "0x100000", "0x20000", "mtdblock1"),
@@ -11,7 +11,7 @@ re_nand_partition = (
                     ("kernel", "0x140000", "0x600000", "mtdblock3"),
                     ("recovery", "0x740000", "0x800000", "mtdblock4"),
                     ("rootfs", "0xf40000", "0x5000000", "mtdblock5"),
-                    ("rootfs", "0x5f40000", "0x20c0000", "mtdblock6"),
+                    ("data", "0x5f40000", "0x20c0000", "mtdblock6"),
 )
 
 local = locals()
